2ffa6db774fa75f6b674d7c220a2cc9d95e7fa6b
Projeto <projeto@pop-os.localdomain>
2026-01-12 20:15:20 -0300

    alrando texto das habilidade

--- assets/assets.js
+++ assets/assets.js
@@ -184,7 +184,7 @@ export const serviceData = [
 
 export const infoList = [
     { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Linguagens', description: 'HTML, CSS, JavaScript, C, python' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Educação', description: 'Cursando ciẽncia da computação(UFOP)' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Educação', description: 'Cursando Ciência da Computação(UFOP)' },
     //{ icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projetos', description: 'Mais de 20 projetos' }
 ];
 
